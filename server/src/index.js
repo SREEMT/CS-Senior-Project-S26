2ecf71a9b64250c1d349d42363c4deeee871b874
SREEMT <tmeers101@gmail.com>
2026-02-02 12:16:11 -0500

    Added files for User Feature #1 - Added testing files for unit tests - Implemented the necessary file structure for the user feature

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+import { userRoutes } from "./routes/user.routes.js"
+
+// Current port for local development.
+// Will implement .env
+const PORT = 3049;
+
+// Serve method to start API
+serve({
+    port: PORT,
+    fetch(req) {
+        const response = userRoutes(req);
+
+        return response ?? new Response("Not Found", {status: 404})
+    }
+});
+
+// Log for testing
+console.log(`API Running on http://localhost:${PORT}`);
